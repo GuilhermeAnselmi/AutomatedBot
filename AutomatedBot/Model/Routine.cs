55a8c136853bedeaf425e8bc41a4d02af828f11b
Guilherme Marques <guianselmi.sa@hotmail.com>
2022-05-17 23:41:17 -0300

    Create model

---
+++ AutomatedBot/Model/Routine.cs
@@ -0,0 +1,16 @@
+﻿using KlusterG.AutoGui;
+
+namespace AutomatedBot.Model
+{
+    internal class Routine
+    {
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public List<Stage> Stage { get; set; }
+    }
+
+    internal class Stage
+    {
+        public ModelProcedure Procedure { get; set; }
+    }
+}
