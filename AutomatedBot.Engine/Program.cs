9ffdcb86c3711d370280b62783973597491758e6
Guilherme Marques <guianselmi.sa@hotmail.com>
2022-05-21 23:21:36 -0300

    Building the engine

--- AutomatedBot.Engine/Program.cs
+++ AutomatedBot.Engine/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace AutomatedBot.Engine;
+﻿using Newtonsoft.Json;
+
+namespace AutomatedBot.Engine;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Program key:value key:value\n");
 
+        string json = "{";
+
+        foreach (string arg in args)
+        {
+            string key = arg.Split(':')[0];
+            string value = arg.Split(':')[1];
+
+            json += $"\"{key}\":\"{value}\",";
+        }
+
+        json += "}";
+
+        Console.WriteLine(json + "\n");
+
+        Dictionary<string, string> obj = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+        Console.WriteLine(obj.Keys.ToList()[0]);
     }
 }
